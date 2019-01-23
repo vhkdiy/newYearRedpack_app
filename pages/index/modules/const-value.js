0258e9555fe94d7ee709c4408ceb84291b425d74
蔡梓锋 <zifeng_cai@163.com>
2019-01-23 15:56:02 +0800

    删除财运指北页面

--- pages/index/modules/const-value.js
+++
@@ -1,7 +0,0 @@
-
-const constValue = {
-  QUESTION_INDEX_BASE_ID: "question_index_base_id_",
-  TOP_ANCHOR_POINT:"top_anchor_point",
-}
-
-export default constValue;
--- pages/index/modules/home-signals.js
+++
@@ -1,7 +0,0 @@
-import signals from './../../../utils/signals/signals.min.js';
-
-const homeSignals = {
-  reset : new signals(),
-}
-
-export default homeSignals;
